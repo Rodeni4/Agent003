dfa07148dba2e7adcc56e0a7f85715042ee13642
Segrey <rodenkosv@gmail.com>
2026-04-16 09:03:33 +0700

    Initial commit

---
+++ src/main/main.ts
@@ -0,0 +1,32 @@
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+
+function createWindow(): void {
+  const win = new BrowserWindow({
+    width: 900,
+    height: 700,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  win.loadFile(path.join(__dirname, '../renderer/index.html'));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
---
+++ src/renderer/renderer.ts
@@ -0,0 +1,7 @@
+const button = document.getElementById('helloButton');
+
+if (button) {
+  button.addEventListener('click', () => {
+    alert('Привет, Друг!');
+  });
+}
